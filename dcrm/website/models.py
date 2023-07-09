0b82c900581fb72b0747ad7e723a85bd2b36a715
Edgardo Cordova <cordovabruch@gmail.com>
2023-07-08 18:36:43 -0600

    commit visualizacion Record

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. no habra necesidad de hacer SQL ya que todo se demine en
+# este modulo
+# con migrate se ejecuta y hace la base de datos: python manage.py makemigrations
+
+class Record(models.Model):     # Model en mayuscula
+    created_at = models.DateTimeField(auto_now_add=True)  # registro del tiempo automatico
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 100)
+    phone =  models.CharField(max_length = 15)
+    address = models.CharField(max_length = 100)
+    city = models.CharField(max_length = 50)
+    state = models.CharField(max_length = 50)
+    zipcode = models.CharField(max_length = 20)
+
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
+
+
+
+
+
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+    # este registro es para la tabla Record
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +24,7 @@ def home(request):
             messages.success(request, 'There was an error logging in, please try again....')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 def logout_user(request):
     logout(request)
@@ -44,3 +48,15 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
     
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):   # pk primery key
+    if request.user.is_authenticated:    # verificacion si esta conectado
+        # lookup record
+        customer_record = Record.objects.get(id=pk) # aqui se pasa el ID
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
+
+
+        
